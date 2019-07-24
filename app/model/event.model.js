0ad48a686d0a8c92b5e72d1a7adc275e7812b8f5
Marlon Alves <maalvesd@microsoft.com>
2019-07-24 14:42:11 -0700

    x

--- app/model/event.model.js
+++ app/model/event.model.js
@@ -1,19 +1,19 @@
 module.exports = (sequelize, Sequelize) => {
-    var Event = sequelize.define('test_event_marlon', { 
+    var Event = sequelize.define('event', {
         event_name: Sequelize.STRING,
-        description: Sequelize.STRING(1000), 
-        date: Sequelize.DATE, 
-        city: Sequelize.STRING, 
+        description: Sequelize.STRING(1000),
+        date: Sequelize.DATE,
+        city: Sequelize.STRING,
         suburb: Sequelize.STRING,
-        location: Sequelize.STRING, 
-        duration_in_minutes: Sequelize.INTEGER, 
-        max_guests: Sequelize.INTEGER, 
-        min_guests: Sequelize.INTEGER, 
-        event_type: Sequelize.STRING, 
+        location: Sequelize.STRING,
+        duration_in_minutes: Sequelize.INTEGER,
+        max_guests: Sequelize.INTEGER,
+        min_guests: Sequelize.INTEGER,
+        event_type: Sequelize.STRING,
         rating: Sequelize.INTEGER,
         owner_email: Sequelize.STRING,
 		participants_emails: Sequelize.ARRAY(Sequelize.STRING)
     });
-	
+
 	return Event;
 }
--- server.js
+++ server.js
@@ -17,12 +17,12 @@ require('./app/route/user.route.js')(app);
 require('./app/route/event.route.js')(app);
 
 
- 
+
 // Create a Server
-var server = app.listen(8081, function () {
- 
+var server = app.listen(8080, function () {
+
   var host = server.address().address
   var port = server.address().port
- 
+
   console.log("App listening at http://%s:%s", host, port)
 })
